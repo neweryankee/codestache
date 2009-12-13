ba997d6f2d0a6ced7388505626e6e9c0c092aa29
Greg <greg@plectix.com>
2009-12-12 21:26:06 -0500

    Buttons with round corners

--- config/compass.rb
+++ config/compass.rb
@@ -5,7 +5,8 @@ project_type = :rails
 project_path = RAILS_ROOT if defined?(RAILS_ROOT)
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "public/stylesheets/compiled"
+css_dir = "public/stylesheets"
 sass_dir = "app/stylesheets"
+images_dir = "public/images" 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
